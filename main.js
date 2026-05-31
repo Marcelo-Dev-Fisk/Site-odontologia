9e822c3e7098b289179313e78bdf9590fcd6ddcd
Marcelo_Dev <marceloaquinofonseca@gmail.com>
2026-05-30 21:44:15 -0300

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-let caixa = document.getElementById('caixa')
-let body = document.querySelector ('body')
-
-caixa.addEventListener('click', ()=>{
-    caixa.classList.toggle('dark')
-    body.classList.toggle('dark')
-})
